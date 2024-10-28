46498df75bb63f253288a04a26c11aea800a351f
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2024-10-28 11:37:36 +0530

    Add js to update display content on button click

--- script.js
+++ script.js
@@ -24,3 +24,10 @@ function operate (operator, num1, num2) {
 }
 
 //create a function that populates display on button click
+
+const display = document.querySelector('.display');
+const btns = document.querySelectorAll('.number');
+
+btns.forEach(btn => {
+	btn.addEventListener('click', () => display.textContent = btn.textContent)
+})
