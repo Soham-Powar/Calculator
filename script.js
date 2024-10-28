6883aa00c6c17eeba82bef946598f53f6ea5f02e
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2024-10-28 11:25:45 +0530

    Add UI to the calculator

---
+++ script.js
@@ -0,0 +1,26 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+   
+function operate (operator, num1, num2) {
+
+}
+
+//create a function that populates display on button click
