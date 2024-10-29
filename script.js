aeb25dcf0d5b0a764b4c297c03e72711ca0cd85d
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2024-10-29 11:10:31 +0530

    Complete the functions to get input from user.

--- script.js
+++ script.js
@@ -20,14 +20,64 @@ function divide() {
 }
    
 function operate (operator, num1, num2) {
+	switch (operator) {
+		case '+':
+			add(num1, num2);
+			break;
+		
+		case '-':
+			subtract(num1, num2);
+			break;
+
+		case 'x':
+			multiply(num1, num2);
+			break;
+
+		case '/':
+			divide(num1, num2);
+			break;
+			
+		default:
+			break;
+	}
 
 }
 
-//create a function that populates display on button click
+//get all the buttons
 
 const display = document.querySelector('.display');
-const btns = document.querySelectorAll('.number');
+const displayResult = document.querySelector('.displayResult');
+const numberBtns = document.querySelectorAll('.number');
+const operationBtns = document.querySelectorAll('.operation');
+const equalBtn = document.querySelector('.equals');
+const clearBtn = document.querySelector('.clear');
+const deleteBtn = document.querySelector('.delete');
 
-btns.forEach(btn => {
-	btn.addEventListener('click', () => display.textContent = btn.textContent)
-})
+//function for clear button
+
+clearBtn.addEventListener('click', () => {
+	display.textContent = '';
+	displayResult.textContent = '';
+});
+//create a function that populates display on number/operation button click
+
+numberBtns.forEach(btn => {
+	btn.addEventListener('click', () => {
+		display.textContent += btn.textContent;
+		displayResult.textContent += btn.textContent;
+	})
+});
+
+operationBtns.forEach(btn => {
+	btn.addEventListener('click', () => {
+		firstNumber = displayResult.textContent;
+		operator = btn.textContent;
+		display.textContent += btn.textContent;
+		displayResult.textContent = '';
+	})
+});
+
+equalBtn.addEventListener('click', () => {
+	secondNumber = displayResult.textContent;
+	operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
+});
