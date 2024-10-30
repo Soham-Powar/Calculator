0f08c91a47de0d7493094e68671f460c6460762b
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2024-10-30 22:58:38 +0530

    Handle continuous expressions with multiple operators

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let firstNumber;
-let secondNumber;
+let secondNumber = '';
 let operator;
 let result;
 
@@ -50,27 +50,67 @@ const equalBtn = document.querySelector('.equals');
 const clearBtn = document.querySelector('.clear');
 const deleteBtn = document.querySelector('.delete');
 
+let gaveOperator = false;
+let secondGiven = false;
+let continuousExp = false
+
 //function for clear button
 
 clearBtn.addEventListener('click', () => {
 	display.textContent = '';
 	displayResult.textContent = '';
 });
+
 //create a function that populates display on number/operation button click
 
 numberBtns.forEach(btn => {
 	btn.addEventListener('click', () => {
+		if(continuousExp) {
+			displayResult.textContent = '';
+			continuousExp = false;
+		}
 		display.textContent += btn.textContent;
 		displayResult.textContent += btn.textContent;
+		if(gaveOperator || secondGiven) {
+			secondNumber += btn.textContent;
+			secondGiven = true;
+			console.log(secondNumber);
+		}
+		gaveOperator = false;
 	})
 });
 
 operationBtns.forEach(btn => {
 	btn.addEventListener('click', () => {
+
+		if(secondGiven) {
+			result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
+			displayResult.textContent = result;
+			firstNumber = result;
+			secondGiven = false;
+			gaveOperator = false;
+			operator = btn.textContent;
+			display.textContent += btn.textContent;
+			continuousExp = true;
+			return;
+		}
+
+		if(gaveOperator) {
+			operator = btn.textContent;
+			display.textContent = display.textContent.slice(0, -1);
+			display.textContent += btn.textContent;
+			return;
+		}
+
+		
 		firstNumber = displayResult.textContent;
 		operator = btn.textContent;
 		display.textContent += btn.textContent;
 		displayResult.textContent = '';
+		gaveOperator = true;
+
+
+
 	})
 });
 
